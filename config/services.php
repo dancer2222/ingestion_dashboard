13722d5f2fe36eebb84b317ac685e6226087066c
sega <tuco.csa@gmail.com>
2017-11-08 19:19:32 +0200

    Modified docker-compose and related things

--- config/services.php
+++ config/services.php
@@ -44,6 +44,6 @@ return [
 	'google' => [
 		'client_id' => env('GOOGLE_CLIENT_ID'),
 		'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-		'redirect_uri' => env('GOOGLE_REDIRECT_URI', 'http://127.0.0.1:7771/oauth2/callback/google'),
+		'redirect_uri' => env('GOOGLE_REDIRECT_URI', url('oauth2/callback/google')),
 	],
 ];
