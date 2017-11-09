b18df19e2a871ae31f48e0e39d9167e606d8ad15
oleksandrd-hyuna <oleksandr.danileyko@hyuna.bb>
2017-11-09 17:48:26 +0200

    PCI-2255: Ignore problems aenetworks with metadata file

--- resources/views/search/sections/infoById/presentInfo/moviesPresentBatchInfo.blade.php
+++ resources/views/search/sections/infoById/presentInfo/moviesPresentBatchInfo.blade.php
@@ -5,15 +5,18 @@
         | <a href="" data-toggle="collapse" data-target="#link">Link to copy</a>
         <div id="link" class="collapse">
             {{ $linkCopy }}
-            <form method="POST" class="form-group" action="{{ action('ExcelController@index') }}">
-                <input type="hidden" id="bucket" name="bucket" value="{{ config('main.links.aws.bucket.movies') }}">
-                <input type="hidden" id="object" name="object" value="{{ $object }}">
-                <input type="hidden" id="title" name="batchTitle" value="{{ $batchInfo['title'] }}">
-                <input type="hidden" id="title" name="title" value="{{ $info['title'] }}">
-                <input type="hidden" id="id" name="id" value="{{ $info['id'] }}">
-                <input type="hidden" name="_token" value="{{csrf_token()}}">
-                <button type="submit" class="btn btn-info">Info by metadata file</button>
-            </form>
+            @if($licensorName == 'aenetworks')
+            @else
+                <form method="POST" class="form-group" action="{{ action('ExcelController@index') }}">
+                    <input type="hidden" id="bucket" name="bucket" value="{{ config('main.links.aws.bucket.movies') }}">
+                    <input type="hidden" id="object" name="object" value="{{ $object }}">
+                    <input type="hidden" id="title" name="batchTitle" value="{{ $batchInfo['title'] }}">
+                    <input type="hidden" id="title" name="title" value="{{ $info['title'] }}">
+                    <input type="hidden" id="id" name="id" value="{{ $info['id'] }}">
+                    <input type="hidden" name="_token" value="{{csrf_token()}}">
+                    <button type="submit" class="btn btn-info">Info by metadata file</button>
+                </form>
+             @endif
         </div>
     </td>
 </tr>
