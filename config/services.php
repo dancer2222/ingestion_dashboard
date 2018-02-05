e02f1d80933418b30666eac5fde0c6ad7fc94dba
SerhiiCherednyk <sergey.cherednik@playster.com>
2018-02-05 12:43:10 +0200

    Getting error with REquest = null was fixed

--- config/services.php
+++ config/services.php
@@ -44,6 +44,6 @@ return [
 	'google' => [
 		'client_id' => env('GOOGLE_CLIENT_ID'),
 		'client_secret' => env('GOOGLE_CLIENT_SECRET'),
-		'redirect_uri' => env('GOOGLE_REDIRECT_URI', url('oauth2/callback/google')),
+		'redirect_uri' => env('GOOGLE_REDIRECT_URI'),
 	],
 ];
