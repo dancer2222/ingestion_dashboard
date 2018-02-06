4265428f2e94a42de924626b7bf683d349189be0
oleksandrd-hyuna <oleksandr.danileyko@hyuna.bb>
2018-02-06 17:20:43 +0200

    PCI-2463:We handle the situation in the absence of geo restrict

--- resources/views/search/infoById.blade.php
+++ resources/views/search/infoById.blade.php
@@ -50,45 +50,7 @@
                 @else
                     @include('search.sections.infoById.presentInfo.optionsNo')
                 @endif
-                <tr>
-                    <td>Geo Restriction</td>
-                    <td>
-                        <button type="button" class="btn btn-outline-success" data-toggle="collapse"
-                                data-target="#country_code">
-                            Show Geo Restriction
-                        </button>
-                        <div id="country_code" class="collapse">
-                            <table class="table table-hover">
-                                <tr>
-                                    <td>
-                                        <b style='color: green'>Active</b> --> @foreach($country_code as $item)
-                                            @if($item['status'] == 'active')
-                                                @if( in_array($item['country_code'],['US', 'CA', 'GB']))
-                                                    <b style='color: red'>{{ $item['country_code'] }}</b>
-                                                @else
-                                                    {{ $item['country_code'] }}
-                                                @endif
-                                            @endif
-                                        @endforeach
-                                    </td>
-                                </tr>
-                                <tr>
-                                    <td>
-                                        <b style='color: green'>Inactive</b> --> @foreach($country_code as $item)
-                                            @if($item['status'] == 'inactive')
-                                                @if( in_array($item['country_code'],['US', 'CA', 'GB']))
-                                                    <b style='color: red'>{{ $item['country_code'] }}</b>
-                                                @else
-                                                    {{ $item['country_code'] }}
-                                                @endif
-                                            @endif
-                                        @endforeach
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                    </td>
-                </tr>
+                @include('search.sections.infoById.geoRestriction')
             </table>
         </div>
     @endif
---
+++ resources/views/search/sections/infoById/geoRestriction.blade.php
@@ -0,0 +1,47 @@
+<tr>
+    <td>Geo Restriction</td>
+    <td>
+        <button type="button" class="btn btn-outline-success" data-toggle="collapse"
+                data-target="#country_code">
+            Show Geo Restriction
+        </button>
+        <div id="country_code" class="collapse">
+            <table class="table table-hover">
+                <tr>
+                    <td>
+                        <b style='color: green'>Active</b> --> @foreach($country_code as $item)
+                            @if(isset($item['status']))
+                                @if($item['status'] == 'active')
+                                    @if( in_array($item['country_code'],['US', 'CA', 'GB']))
+                                        <b style='color: red'>{{ $item['country_code'] }}</b>
+                                    @else
+                                        {{ $item['country_code'] }}
+                                    @endif
+                                @endif
+                            @else
+                                {{ $item }}
+                            @endif
+                        @endforeach
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <b style='color: green'>Inactive</b> --> @foreach($country_code as $item)
+                            @if(isset($item['status']))
+                                @if($item['status'] == 'inactive')
+                                    @if( in_array($item['country_code'],['US', 'CA', 'GB']))
+                                        <b style='color: red'>{{ $item['country_code'] }}</b>
+                                    @else
+                                        {{ $item['country_code'] }}
+                                    @endif
+                                @endif
+                            @else
+                                {{ $item }}
+                            @endif
+                        @endforeach
+                    </td>
+                </tr>
+            </table>
+        </div>
+    </td>
+</tr>
