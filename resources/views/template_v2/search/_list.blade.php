e82a722236eca30f4640d11c9e0fb2569a021684
oleksandrd-hyuna <oleksandr.danileyko@hyuna.bb>
2018-09-17 14:43:44 +0300

    add show info by Licensor

--- resources/views/template_v2/search/_list.blade.php
+++ resources/views/template_v2/search/_list.blade.php
@@ -8,16 +8,16 @@
                     <tr>
                         <th scope="col">Id</th>
                         <th scope="col">Title</th>
-                        <th scope="col">Licensor</th>
+                        <th scope="col" class="text-left">Licensor</th>
                     </tr>
                 </thead>
 
                 <tbody>
                 @foreach($list as $item)
                     <tr>
-                        <td><a href="{{ route('reports.show', ['mediaType' => $mediaType, 'id' => $item->id]) }}">{{ $item->id }}</a></td>
-                        <td><a href="{{ route('reports.show', ['mediaType' => $mediaType, 'id' => $item->id]) }}">{{ $item->title }}</a></td>
-                        <td><a href="{{ route('reports.show', ['mediaType' => $mediaType, 'id' => $item->id]) }}">{{ $item->licensor->name ?? ''}}</a></td>
+                        <td><a title="Click to see more info about this Id" href="{{ route('reports.show', ['mediaType' => $mediaType, 'id' => $item->id]) }}">{{ $item->id }}</a></td>
+                        <td><a title="Click to see more info about this Title" href="{{ route('reports.show', ['mediaType' => $mediaType, 'id' => $item->id]) }}">{{ $item->title }}</a></td>
+                        <td class="text-left"><a title="Click to see more info about this Licensor" href="{{ route('licensors.show', ['id' => $item->licensor_id]) }}">{{ $item->licensor->name ?? ''}}</a></td>
                     </tr>
                 @endforeach
                 </tbody>
