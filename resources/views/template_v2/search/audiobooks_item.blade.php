30f2df663108a2d5268a29a2b17b10682331a57a
oleksandrd-hyuna <oleksandr.danileyko@hyuna.bb>
2018-09-17 15:58:13 +0300

    PCI-2746: add link by show info about licensor

--- resources/views/template_v2/search/audiobooks_item.blade.php
+++ resources/views/template_v2/search/audiobooks_item.blade.php
@@ -61,7 +61,11 @@
 
 <div class="mb-3 border-bottom">
     <b>Licensor:</b>
-    <span class="float-right">{{ $item->licensor_id }} - {{ $item->licensor->name }}</span>
+    <span class="float-right">
+         <a title="show info about this licensor" href="{{ route('licensors.show', ['id' => $item->licensor_id]) }}" target="_blank">
+            {{ $item->licensor_id }} [{{ $item->licensor->name }}]
+         </a>
+    </span>
 </div>
 
 <div class="mb-3 border-bottom">
