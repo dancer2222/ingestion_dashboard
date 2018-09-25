89e9ca7c2ae7813763bdb725587c5e0b3c88b98d
SerhiiCherednyk <sergey.cherednik@playster.com>
2018-09-25 19:23:05 +0300

    PCI-2758 Added helper function 'excelToArray' and implemented it

--- resources/views/search/metadata.blade.php
+++ resources/views/search/metadata.blade.php
@@ -1,28 +1,24 @@
-@extends('layouts.main')
-
-@section('title', 'Folders')
-
-@section('content')
-    <table class="table table-hover" border="2px">
+<table class="table table-hover" border="2px">
+    <tr>
+        @if(isset($messages[0]))
+            @foreach($messages[0] as $item => $a)
+                @if($item !== 'description')
+                <th style="background-color: #2ca02c">
+                    {{ $item }}
+                </th>
+                @endif
+            @endforeach
+        @endif
+    </tr>
+    @foreach($messages as $message)
         <tr>
-            @if(isset($messages[0]))
-                @foreach($messages[0] as $item => $a)
-                    <th style="background-color: #2ca02c">
-                        {{ $item }}
-                    </th>
-                @endforeach
-            @endif
+            @foreach($message as $name => $value)
+                @if($name !== 'description')
+                <td>
+                    <p style="font-size: 13px">{{ $value }}</p>
+                </td>
+                @endif
+            @endforeach
         </tr>
-        @foreach($messages as $message)
-            <tr>
-                @foreach($message as $value)
-                    <td>
-                        <p style="font-size: 13px">{{ $value }}</p>
-                    </td>
-                @endforeach
-            </tr>
-        @endforeach
-    </table>
-
-    <a class="btn btn-info" href="{{ URL::previous() }}">back</a>
-@endsection
+    @endforeach
+</table>
